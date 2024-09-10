1cce3a6caca385cee0f610fd70cbd07ad52b8a00
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-09-10 20:10:15 +0100

    We create the files dinamycally

--- diff_cli/diff_cli_test.go
+++ diff_cli/diff_cli_test.go
@@ -3,14 +3,42 @@ package diff_cli_test
 import (
 	cli "github.com/MiyamotoAkira/diffcli/diff_cli"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
+func createFile(fileName string, fileContent []string) {
+	f, err := os.Create(fileName)
+	if err != nil {
+		panic("Creating file " + fileName)
+	}
+	defer f.Close()
+
+	for _, line := range fileContent {
+		_, err := f.WriteString(line + "\n")
+		if err != nil {
+			panic("Writing file " + fileName)
+		}
+	}
+}
+
 func TestCompareTwoFiles(t *testing.T) {
-	var file1Name = "file1.txt"
-	var file2Name = "file2.txt"
+	folder, err := os.MkdirTemp("./", "temp")
+	if err != nil {
+		panic("Error creating directory")
+	}
+	var file1Name = folder + "/" + "file1.txt"
+	var file2Name = folder + "/" + "file2.txt"
+	createFile(file1Name, []string{"abc", "def", "ghi"})
+	createFile(file2Name, []string{"abc", "dzf"})
 	var result string
 
 	result = cli.CompareFiles(file1Name, file2Name)
 	assert.Equal(t, "- def\n+ dzf\n- ghi\n+", result)
+
+	err = os.RemoveAll(folder)
+
+	if err != nil {
+		panic("Issue removing folder " + folder)
+	}
 }
